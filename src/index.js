542033fb2dad8e3a0c1c4f608b2d8c30505f22a4
Nikolai <nikolai.laevskii@yandex.ru>
2019-01-29 22:15:28 +0300

    Добавлены глобальные стили

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from '@/App'
 import 'normalize.css'
 import '@/fonts/open-sans/stylesheet.css'
 import '@/fonts/icons/stylesheet.css'
+import '@/index.css'
 
 // eslint-disable-next-line
 new Vue({
