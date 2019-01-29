798ed00d0c99d789682f5e12c41e1304c6c52bb8
Nikolai <nikolai.laevskii@yandex.ru>
2019-01-29 23:29:07 +0300

    Добавлены основные секции

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="main">
+  <div class="app">
     <Header />
+    <MainSection />
+    <FooterSection />
   </div>
 </template>
 
 <script>
 import Header from '@@/Header'
+import MainSection from '@@/MainSection'
+import FooterSection from '@@/FooterSection'
 
 export default {
   components: {
-    Header
+    Header,
+    MainSection,
+    FooterSection
   }
 }
+
 </script>
 
 <style lang="stylus" scoped>
+.app
+  display flex
+  flex-direction column
+  min-height 100vh
 </style>
---
+++ src/components/MainSection/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class='main-section'></main>
+</template>
+
+<style lang="stylus" scoped>
+.main-section
+  flex-grow 1
+</style>
