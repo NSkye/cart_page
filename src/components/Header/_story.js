9f18ee12ad7eb59df73092d2efefb75bb046c966
Nikolai <nikolai.laevskii@yandex.ru>
2019-01-31 04:28:08 +0300

    Убрана _story хэдера за ненадобностью

--- src/components/Header/_story.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
-import Header from '@@/Header'
-
-Vue.component('c-header', Header)
-
-storiesOf('Header', module)
-  .add('header', () => '<div><c-header/></div>')
